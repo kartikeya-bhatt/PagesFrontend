d0ecb6d8dca496b1e654954d409ae05093b9f906
kartikeya-bhatt <krtky020@gmail.com>
2021-10-16 22:25:59 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,135 @@
+
+import Posts from './components/posts'
+import React, {useState} from "react";
+import './App.css';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import { Alert } from 'react-native';
+
+function MyVerticallyCenteredModal(props)  {
+
+    const [inputs, setInputs] = useState({});
+
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs(values => ({...values, [name]: value}))
+    }
+
+    const handleSubmit = async (event) => {
+        const obj = {
+            user: inputs.username
+        }
+
+        const request = await fetch ("http://127.0.0.1:8787/check-users", {
+            method: 'POST',
+            body: JSON.stringify(obj)
+        });
+        const value = request.body
+
+        if(value != null) {
+            const url = "https://p-roses-financing-malpractice.trycloudflare.com/auth/" + inputs.username
+            await fetch(url, {method: "GET"})
+        }
+        else {
+            const url = "https://p-roses-financing-malpractice.trycloudflare.com/verify"
+            const resp = await fetch(url, {method: "GET"})
+            if(resp.body.toString() !== inputs.username) {
+                Alert.alert("You do not have permission to post with this username!")
+                return
+            }
+        }
+
+        const post = {
+            title: inputs.title,
+            username: inputs.username,
+            content: inputs.content,
+        };
+
+        const url = "http://127.0.0.1:8787/posts"
+        await fetch(url, {
+            method: "POST",
+            body: JSON.stringify(post),
+        });
+
+        window.location.reload(false);
+    }
+
+    return (
+        <Modal
+            {...props}
+            size="md"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+        >
+            <Modal.Header>
+                <Modal.Title id="contained-modal-title-vcenter">
+                    Submit a post!
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <div className="col-16 col-lg-6 offset-lg-3">
+                    <form onSubmit={handleSubmit}>
+                        <input
+                            className="form-control my-3"
+                            placeholder="Title"
+                            type="text"
+                            name="title"
+                            value={inputs.title || ""}
+                            onChange={handleChange}
+                        />
+                        <textarea
+                            className="form-control my-3"
+                            placeholder="Content"
+                            value={inputs.content || ""}
+                            name="content"
+                            onChange={handleChange}
+                        >
+                        </textarea>
+                        <input
+                            className="form-control my-3"
+                            placeholder="Username"
+                            type="text"
+                            name="username"
+                            value={inputs.username || ""}
+                            onChange={handleChange}
+                        />
+                    </form>
+                </div>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button className="btn btn-primary float-end" data-dismiss="modal" onClick={() => handleSubmit()}>Submit</Button>
+            </Modal.Footer>
+        </Modal>
+    );
+}
+
+const App = () => {
+    const [modalShow, setModalShow] = React.useState(false);
+
+    return (
+        <div className="container">
+            <div>
+
+                <div className="pt-3 col-12 col-lg-6 offset-lg-3">
+                    <h1 className="text-center">My Social Media</h1>
+                </div>
+
+                <div className="row pt-3">
+                    <div className="text-center">
+                        <Button variant="primary" onClick={() => setModalShow(true)}>
+                            Make a post
+                        </Button>
+                    </div>
+                </div>
+                <MyVerticallyCenteredModal
+                    show={modalShow}
+                    onHide={() => setModalShow(false)}
+                />
+                <Posts />
+            </div>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/posts.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text } from 'react-native';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Posts = () => {
+
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        const getPosts = async () => {
+            const url = "http://127.0.0.1:8787/posts"
+            const resp = await fetch(url, {
+                method: "GET",
+            })
+            const postsResp = await resp.json();
+            setPosts(postsResp);
+        };
+        getPosts();
+    }, []);
+
+    const styles = StyleSheet.create({
+        titleText: {
+            fontSize: 20,
+            fontWeight: "bold"
+        }
+    });
+
+    return (
+            <div className="row pt-5">
+                <div className="col-12 col-lg-6 offset-lg-3">
+                    {posts.map((post) => {
+                        return (
+                            <div className="card my-3">
+                                <div className="card-header">
+                                    <Text style={styles.titleText}>
+                                        {post.title}
+                                    </Text>
+                                </div>
+                                <div className="card-body">
+                                    {post.content}
+                                </div>
+                                <div className="card-footer">
+                                    <i>{post.username}
+                                    </i>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
+    );
+
+}
+
+export default Posts;
